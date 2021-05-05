5a95b33ecaeb8c2bd953f8d59b063fd88b92b18c
Daniel Ramirez <notused@notused.net>
2021-05-05 10:18:46 -0700

    Adds axios module, componentDidMount funciton with API call for deck data retrieval

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React, { Component } from "react";
 import Title from "./components/Title/Title";
+const axios = require("axios");
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      decks: [],
+      mainEndpoint: "http://localhost:5000/api/decks/",
+      decks: "",
     };
   }
+
+  componentDidMount() {
+    this.getAllDecks(this.state.mainEndpoint);
+    console.log(this.state.decks);
+  }
+
+  async getAllDecks() {
+    try {
+      const response = await this.getDeckDataAPICall(this.state.mainEndpoint);
+      this.setState({
+        decks: response.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getDeckDataAPICall(endpoint) {
+    return new Promise((res, rej) => {
+      const response = axios.get(endpoint);
+      if (response != null) {
+        res(response);
+      } else {
+        rej(new Error(`Unable to access data from ${endpoint} `));
+      }
+    });
+  }
+
   render() {
     return (
       <div>
-        <Title />
+        <Title testPro={this.state.decks} />
       </div>
     );
   }
